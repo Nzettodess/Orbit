c63c231953f7647728014325f33a16826f1e6500
Nzettodess <angkangheng@hotmail.com>
2025-11-23 23:09:48 +0800

    update fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_fonts/google_fonts.dart'; // Google Fonts for NotoSans SC
 import 'firebase_options.dart';
 import 'home.dart'; // Only home.dart, login is imported inside home
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  
+
   // Disable persistence to avoid "Unexpected state" errors from corrupted cache
   FirebaseFirestore.instance.settings = const Settings(persistenceEnabled: false);
 
@@ -25,6 +26,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        textTheme: GoogleFonts.notoSansScTextTheme(),
       ),
       home: const HomeWithLogin(), // Wrapper for home + login
     );
